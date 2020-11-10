7a328e09934d957e016df12e5f4505aa7ccbeead
Maquiner <jaimerz011235813@gmail.com>
2020-11-10 20:26:45 +0000

    more methods added

---
+++ mappers/mapper_CMBK.py
@@ -0,0 +1,68 @@
+from mapper_base import MapperBase
+from astropy.io import fits
+from astropy.table import Table
+import pandas as pd
+import numpy as np
+import healpy as hp
+import pymaster as nmt
+import os
+
+class MapperCMBK(MapperBase):
+    def __init__(self, config):
+        self.config = config
+
+        self.klm = []
+        self.mask = []
+        self.noise = []
+        
+        for file_klm, file_mask, file_noise in zip(self.config['data_klm'],
+                self.config['data_mask'], self.config['data_noise']):
+            
+            if not os.path.isfile(file_klm):
+                raise ValueError(f"File {file_klm} not found")
+            self.klm = hp.read_alm(file_klm)
+                
+            if not os.path.isfile(file_mask):
+                raise ValueError(f"File {file_mask} not found")
+            with fits.open(file_mask,  ignore_missing_end=True) as f:
+                self.mask= Table.read(f).to_pandas()
+                
+            if not os.path.isfile(file_noise):
+                raise ValueError(f"File {file_noise} not found")
+            self.noise = pd.read_table(file_noise, names=['l','Nl','Nl+Cl'], sep=" ", encoding='utf-8')
+            
+        self.nside = config['nside']
+        self.r = hp.rotator.Rotator(coord=['G','C'])
+        
+        self.k_map      = None
+        self.nl_coupled = None
+        self.mask_map   = None
+        self.nmt_field  = None
+        self.cl_fid     = None
+
+
+    def get_signal_map(self):
+        if self.k_map is None:
+            self.k_map  = self.r.rotate_alm(self.klm)
+            self.k_map =  hp.sphtfunc.alm2map(self.k_map, self.nside)
+        return self.k_map
+
+    def get_mask(self):
+        if self.mask_map is None:
+            self.mask_map = self.r.rotate_map_pixel(self.mask['SIGNAL'].values)
+        return self.mask_map
+
+    def get_nmt_field(self, signal, mask):
+        if self.nmt_field is None:
+            self.nmt_field = nmt.NmtField(mask, [signal])
+        return self.nmt_field
+
+    def get_nl_coupled(self):
+        if self.nl_coupled is None:
+             self.nl_coupled = self.noise['Nl'].values
+        return np.array([self.nl_coupled])
+    
+    def get_cl_fiducial(self):
+        if self.cl_fid is None:
+             self.cl_fid = self.noise['Nl+Cl'].values - self.noise['Nl'].values
+        return self.cl_fid
