f2d7c35c2aafef6e590af57490d2e2382c4f73d2
Maquiner <jaimerz011235813@gmail.com>
2020-11-27 13:15:01 +0000

    debugged up to nmt field, needs testings

--- mappers/mapper_KV450.py
+++ mappers/mapper_KV450.py
@@ -25,30 +25,37 @@ class MapperKV450(MapperBase):
         '5':[0.9, 1.2]}
         
         self.cat_data = []
-        self.nz = np.loadtxt(self.config['file_nz'], unpack=True)
         for file_data in self.config['data_catalogs']:
             if not os.path.isfile(file_data):
                 raise ValueError(f"File {file_data} not found")
             with fits.open(file_data) as f:
-                cat = pd.DataFrame()
-                cat = Table.read(f, format='fits')
-                cat = data[self.column_names]
-                self.cat_data.append()
+                self.cat_data.append(Table.read(f).to_pandas())
+                #cat = pd.DataFrame()
+                #cat = Table.read(f), format='fits')
+                #cat = cat[self.column_names]
+                #self.cat_data.append(cat)
         
+        print('Catalogs loaded')
         self.nside = config['nside']
         self.npix = hp.nside2npix(self.nside)
-        self.bin = config['bin']
-        self.z_edges = self.bin_edges['{}'.format(self.bin)]
-
+        self.zbin = config['zbin']
+        self.z_edges = self.zbin_edges['{}'.format(self.zbin)]
+        print('z edges ' , self.z_edges)
         self.cat_data = pd.concat(self.cat_data)
+        print('data concatenated')
         self.cat_data = self._bin_z(self.cat_data)
+        print('Data binned')
         self._remove_additive_bias()
-        self._remove_multiplicative_bias()
-        
-        self.w_data = self._get_weights(self.cat_data) 
+        print('Additive biased removed')
+        self._remove_multiplicative_bias() 
+        print('Multiplicative bias removed')
 
-        self.dndz       = None
-        self.delta_map  = None
+        self.dndz = np.loadtxt(self.config['file_nz'], unpack=True)
+        self.shear_map  = None
+        self.psf_map    = None
+        self.star_map   = None
+        self.star_mask  = None
+        self.galaxy_mask= None
         self.nl_coupled = None
         self.nmt_field  = None
 
@@ -68,8 +75,8 @@ class MapperKV450(MapperBase):
         # Values from Table 2 of 1812.06076 (KV450 cosmo paper)
         m = (-0.017, -0.008, -0.015, 0.010, 0.006)
         sel_gals = self.cat_data['SG_FLAG'] == 1
-        self.cat_data[sel_gals]['bias_corrected_e1'] /= 1 + m[self.bin]
-        self.cat_data[sel_gals]['bias_corrected_e2'] /= 1 + m[self.bin]
+        self.cat_data[sel_gals]['bias_corrected_e1'] /= 1 + m[self.zbin]
+        self.cat_data[sel_gals]['bias_corrected_e2'] /= 1 + m[self.zbin]
     
     def _get_galaxy_data(self):
         sel = self.cat_data['SG_FLAG'] == 1
@@ -80,65 +87,77 @@ class MapperKV450(MapperBase):
         return self.cat_data[sel]
     
     def get_shear_map(self, nside=None):
-        if nside is None:
-            nside = self.nside
-        npix = hp.nside2npix(nside)    
-        ipix = hp.ang2pix(nside, self.cat_data['RA'], self.cat_data['DEC'],
-                          lonlat=True)
-        
-        we1 = np.bincount(ipix, weights=self.cat_data['weight']*self.cat_data['bias_corrected_e1'], minlength=npix)
-        
-        we2 = np.bincount(ipix, weights=self.cat_data['weight']*self.cat_data['bias_corrected_e2'], minlength=npix)
-        
-        w2s2 = np.bincount(ipix, weights=self.cat_data['weight']**2 * 0.5 * (self.cat_data['bias_corrected_e1']**2 + self.cat_data['bias_corrected_e2']**2), minlength=npix)
+        if self.shear_map is None:
+            if nside is None:
+                nside = self.nside
+            npix = hp.nside2npix(nside)    
+            phi = np.radians(self.cat_data['ALPHA_J2000'])
+            theta = np.radians(90 - self.cat_data['DELTA_J2000'])
+            ipix = hp.ang2pix(nside, theta, phi)
 
-        return we1, we2, w2s2
+            we1 = np.bincount(ipix, weights=self.cat_data['weight']*self.cat_data['bias_corrected_e1'], minlength=npix)
+
+            we2 = np.bincount(ipix, weights=self.cat_data['weight']*self.cat_data['bias_corrected_e2'], minlength=npix)
+
+            w2s2 = np.bincount(ipix, weights=self.cat_data['weight']**2 * 0.5 * (self.cat_data['bias_corrected_e1']**2 + self.cat_data['bias_corrected_e2']**2), minlength=npix)
+            self.shear_map = [we1, we2, w2s2]
+        return self.shear_map
 
     def get_psf_map(self, nside=None):
-        if nside is None:
-            nside = self.nside
-        npix = hp.nside2npix(nside)    
-        ipix = hp.ang2pix(nside, self.cat_data['RA'], self.cat_data['DEC'],
-                          lonlat=True)
-        
-        we1 = np.bincount(ipix, weights=self.cat_data['weight']*self.cat_data['PSF_e1'], minlength=npix)
-        
-        we2 = np.bincount(ipix, weights=self.cat_data['weight']*self.cat_data['PSF_e2'], minlength=npix)
-        
-        w2s2 = np.bincount(ipix, weights=self.cat_data['weight']**2 * 0.5 * (self.cat_data['PSF_e1']**2 + self.cat_data['PSF_e2']**2), minlength=self.npix)
+        if self.psf_map is None:
+            if nside is None:
+                nside = self.nside
+            npix = hp.nside2npix(nside)    
+            phi = np.radians(self.cat_data['ALPHA_J2000'])
+            theta = np.radians(90 - self.cat_data['DELTA_J2000'])
+            ipix = hp.ang2pix(nside, theta, phi)
 
-        return we1, we2, w2s2
+            we1 = np.bincount(ipix, weights=self.cat_data['weight']*self.cat_data['PSF_e1'], minlength=npix)
+
+            we2 = np.bincount(ipix, weights=self.cat_data['weight']*self.cat_data['PSF_e2'], minlength=npix)
+
+            w2s2 = np.bincount(ipix, weights=self.cat_data['weight']**2 * 0.5 * (self.cat_data['PSF_e1']**2 + self.cat_data['PSF_e2']**2), minlength=self.npix)
+            self.psf_map = [we1, we2, w2s2]
+        return self.psf_map
 
     def get_star_map(self, nside=None):
-        if nside is None:
-            nside = self.nside
-        npix = hp.nside2npix(nside)    
-        ipix = hp.ang2pix(nside, self.cat_data['RA'], self.cat_data['DEC'],
-                          lonlat=True)
-        
-        return np.bincount(ipix, minlength=self.npix)
+        if self.star_map is None:
+            if nside is None:
+                nside = self.nside
+            npix = hp.nside2npix(nside)    
+            phi = np.radians(self.cat_data['ALPHA_J2000'])
+            theta = np.radians(90 - self.cat_data['DELTA_J2000'])
+            ipix = hp.ang2pix(nside, theta, phi)
+            
+            self.star_map = np.bincount(ipix, minlength=self.npix)
+        return self.star_map
 
     def get_star_mask(self, nside=None, w2=True):
-        if nside is None:
-            nside = self.nside
-        npix = hp.nside2npix(nside)    
-        ipix = hp.ang2pix(nside, self.cat_data['RA'], self.cat_data['DEC'],
-                          lonlat=True)
-        
-        mask = np.bincount(ipix, weights=self.cat_data['weight'], minlength=npix)
-        if not w2:
-            return mask
-        w2 = np.bincount(ipix, weights=self.cat_data['weight']**2, minlength=npix)
-        return mask, w2
+        if self.star_mask is None:
+            if nside is None:
+                nside = self.nside
+            npix = hp.nside2npix(nside)  
+            phi = np.radians(self.cat_data['ALPHA_J2000'])
+            theta = np.radians(90 - self.cat_data['DELTA_J2000'])
+            ipix = hp.ang2pix(nside, theta, phi)
+            
+            self.star_mask = [np.bincount(ipix, weights=self.cat_data['weight'], minlength=npix)]
+            if w2:
+                w2 = np.bincount(ipix, weights=self.cat_data['weight']**2, minlength=npix)
+                self.star_mask.append(w2)
+        return self.star_mask
 
     def get_galaxy_mask(self, nside=None):
-        if nside is None:
-            nside = self.nside
-        npix = hp.nside2npix(nside)    
-        ipix = hp.ang2pix(nside, self.cat_data['RA'], self.cat_data['DEC'],
-                          lonlat=True)
-
-        return np.bincount(ipix, weights=self.cat_data['weight'], minlength=npix)
+        if self.galaxy_mask is None:
+            if nside is None:
+                nside = self.nside
+            npix = hp.nside2npix(nside)    
+            phi = np.radians(self.cat_data['ALPHA_J2000'])
+            theta = np.radians(90 - self.cat_data['DELTA_J2000'])
+            ipix = hp.ang2pix(nside, theta, phi)
+            
+            self.galaxy_mask = np.bincount(ipix, weights=self.cat_data['weight'], minlength=npix)
+        return self.galaxy_mask
 
     def get_nmt_field(self):
         if self.nmt_field is None:
