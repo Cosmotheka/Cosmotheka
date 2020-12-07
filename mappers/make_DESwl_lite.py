c50217ca90dcfa9ddc9dcf810474bad7ea7b7e93
carlosggarcia <carlos.garcia-garcia@physics.ox.ac.uk>
2020-12-07 18:34:26 +0000

    fixed bin error and make it an argument

--- mappers/make_DESwl_lite.py
+++ mappers/make_DESwl_lite.py
@@ -1,28 +1,18 @@
-from mapper_base import MapperBase
-from astropy.io import fits
-from astropy.table import Table
-
-import mapper_DESwl
-import pyccl as ccl
-import numpy as np
-import pylab as plt
-import pymaster as nmt
-import os
+## Executed with:
+# addqueue -n 1x10 -m 10 -s -c "mins" -q berg /usr/bin/python3 make_DESwl_lite.py 4
+import sys
+sys.path.append('/mnt/zfsusers/gravityls_3/codes/DEScls/')
+import mappers.mapper_DESwl_Carlos as mapper_DESwl_Carlos
 
 nside = 512
 
 path_data = '/mnt/extraspace/damonge/S8z_data/DES_data/shear_catalog/'
 
-config =  {'zbin_cat': path_data + 'y1_source_redshift_binning_v1.fits', 
+config =  {'zbin_cat': path_data + 'y1_source_redshift_binning_v1.fits',
         'data_cat':  path_data + 'mcal-y1a1-combined-riz-unblind-v4-matched.fits',
          'file_nz': path_data + 'y1_redshift_distributions_v1.fits',
          'nside': nside,
-         'bin': bin,
+         'bin': int(sys.argv[1]),
          'mask_name': 'name'}
 
-mapper_DESwl.MapperDESwl(config)
-
-
-            
-        
-        
+mapper_DESwl_Carlos.MapperDESwl(config)
