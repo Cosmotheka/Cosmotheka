70433250e0615513704ba7a701f4b7099c0e5289
David Alonso <dam.phys@gmail.com>
2021-08-05 12:00:46 +0100

    2MPZ mapper (#71)
    * 2mpz mapper
    
    * DIR implemented
    
    * flake2
    
    * xcell/mappers/mapper_WIxSC.py
    
    * wisc
    
    * polynomial order for star correction
    
    * example yaml
    
    * all done
    
    * bug in nl_coupled
    
    * something's up with DELS
    
    * DELS back
    
    * modes
    
    * test DIR
    
    * test DIR
    
    * comment
    
    * radians mapping
    
    * 2mpz tests
    
    * all tests ready
    
    * missing sklearn
    
    * Jaime's comments
    
    * edges in test

--- xcell/mappers/__init__.py
+++ xcell/mappers/__init__.py
@@ -7,8 +7,10 @@ from .mapper_KV450 import MapperKV450
 from .mapper_KiDS1000 import MapperKiDS1000
 from .mapper_P18CMBK import MapperP18CMBK
 from .mapper_DELS import MapperDELS
+from .mapper_2MPZ import Mapper2MPZ
+from .mapper_WIxSC import MapperWIxSC
 from .mapper_dummy import MapperDummy
-from .utils import get_map_from_points
+from .utils import get_map_from_points, get_DIR_Nz
 
 
 def mapper_from_name(name):
