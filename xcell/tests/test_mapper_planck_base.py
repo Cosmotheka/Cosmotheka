8af76cc4cfbad1d7a61514269123bfeb0504be38
David Alonso <dam.phys@gmail.com>
2021-08-11 10:19:02 +0100

    new test

--- xcell/tests/test_mapper_planck_base.py
+++ xcell/tests/test_mapper_planck_base.py
@@ -1,6 +1,7 @@
 import xcell as xc
 import numpy as np
 import pytest
+import healpy as hp
 
 
 def get_config():
@@ -11,6 +12,7 @@ def get_config():
          'file_gp_mask': 'xcell/tests/data/mask1.fits',
          'file_sp_mask': 'xcell/tests/data/mask2.fits',
          'gal_mask_mode': '0.2',
+         'beam_fwhm_arcmin': 0.,
          'nside': 32}
     return c
 
@@ -40,22 +42,24 @@ def test_get_nl_coupled(m):
     assert np.mean(nl) < 0.001
 
 
-@pytest.mark.parametrize(['m', 'n'],
-                         [[xc.mappers.MapperP15tSZ(get_config()),
-                           xc.mappers.MapperP15tSZ(get_config())],
-                         [xc.mappers.MapperP18SMICA(get_config()),
-                          xc.mappers.MapperP18SMICA(get_config())],
-                         [xc.mappers.MapperP15CIB(get_config()),
-                          xc.mappers.MapperP15CIB(get_config())]])
-def test_get_cl_coupled(m, n):
-    m.file_map = 'xcell/tests/data/map_auto_test.fits'
-    m.cl_mode = 'Auto'
-    n.file_map = 'xcell/tests/data/map_auto_test.fits'
-    n.cl_mode = 'Cross'
-    nl = m.get_nl_coupled()
-    cl = m.get_cl_coupled()
-    cl_signal = n.get_cl_coupled()
-    assert nl.shape == (1, 3*32)
-    assert cl.shape == (1, 3*32)
-    assert cl_signal.shape == (1, 3*32)
-    assert abs(np.mean((cl[0]-nl[0]-cl_signal[0])[20:])) < 0.001
+@pytest.mark.parametrize('cls', [xc.mappers.MapperP15tSZ,
+                                 xc.mappers.MapperP18SMICA,
+                                 xc.mappers.MapperP15CIB])
+def test_get_cl_coupled(cls):
+    conf = get_config()
+    conf['file_map'] = 'xcell/tests/data/map_auto_test.fits'
+    m = cls(conf)
+    mask = m.get_mask()
+    cl_cross = m.get_cl_coupled()[0]
+    nl_diff = m.get_nl_coupled()[0]
+    m1 = hp.read_map("xcell/tests/data/hm1_map.fits", verbose=False)
+    m2 = hp.read_map("xcell/tests/data/hm2_map.fits", verbose=False)
+    cl_cross_bm = hp.anafast(m1*mask, m2*mask, iter=0)
+    nl_diff_bm = hp.anafast(0.5*(m1-m2)*mask, iter=0)
+    # Typical C_ell value for comparison (~1E-3 in this case)
+    cl_cross_scale = np.mean(np.fabs(cl_cross_bm[2:]))
+    nl_diff_scale = np.mean(np.fabs(nl_diff_bm[2:]))
+    assert np.allclose(cl_cross, cl_cross_bm,
+                       rtol=0, atol=1E-10*cl_cross_scale)
+    assert np.allclose(nl_diff, nl_diff_bm,
+                       rtol=0, atol=1E-10*nl_diff_scale)
