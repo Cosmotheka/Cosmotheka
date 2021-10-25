0dbd67a54b0870663a5d6d33659736f40e2c5846
Felipe Oliveira Franco <felipeoliveirafranco@felipes-mbp.home>
2021-10-25 09:18:28 +0100

    new update for mapper_NVSS.py

--- xcell/mappers/mapper_NVSS.py
+++ xcell/mappers/mapper_NVSS.py
@@ -17,108 +17,50 @@ class MapperNVSS(MapperBase):
            'n_jk_dir': 100,
            'mask_name': 'mask_2MPZ'}
         """
+        
         self._get_defaults(config)
-        self.z_edges = config.get('z_edges', [0, 0.5])
-        self.ra_name, self.dec_name = self._get_coords(config)
+        
+        
+        ## New
+        self.file_sourcemask = config['mask_sources']
+        
+        self.ra_name = 'RAJ2000'
+        self.dec_name = 'DEJ2000'
 
         self.cat_data = None
         self.npix = hp.nside2npix(self.nside)
 
         # Angular mask
-        self.dndz = None
         self.mask = None
-        self.dndz = None
         self.delta_map = None
         self.nl_coupled = None
-        self.mask = None
-
-    def _get_coords(self, config):
-        coords = config.get('coordinates', 'G')
-        if coords == 'G':  # Galactic
-            return 'L', 'B'
-        elif coords == 'C':  # Celestial/Equatorial
-            return 'SUPRA', 'SUPDEC'
-        else:
-            raise NotImplementedError(f"Unknown coordinates {coords}")
 
     def get_catalog(self):
         if self.cat_data is None:
             file_data = self.config['data_catalog']
-            if not os.path.isfile(file_data):
-                raise ValueError(f"File {file_data} not found")
+    
             self.cat_data = fitsio.read(file_data)
-            self.cat_data = self._bin_z(self.cat_data)
-            self.cat_data = self._mask_catalog(self.cat_data)
+            
+            # Galactic coordinates
+            r = hp.Rotator(coord=['C','G']) 
+            GLON,GLAT = r(self.cat_data['RAJ2000'], self.cat_data['DEJ2000'],lonlat=True)
+            
+            self.cat_data['GLON'] = GLON
+            self.cat_data['GLAT'] = GLAT
+            
+            # Angular and flux conditions    
+            self.cat_data = self.cat_data[(self.cat_data['DEJ2000'] > -40) &
+              (self.cat_data['S1_4'] > 10) & 
+              (self.cat_data['S1_4'] < 1000) & 
+              (np.fabs(self.cat_data['GLAT']) > 5)]
 
         return self.cat_data
-
-    def _check_rerun_file_exists(self, fname):
-        path_i = self.config.get('path_rerun', None)
-        if path_i is None:
-            return False, None
-        else:
-            fname_full = os.path.join(path_i, fname)
-            return os.path.isfile(fname_full), fname_full
-
-    def _mask_catalog(self, cat):
-        self.mask = self.get_mask()
-        ipix = hp.ang2pix(self.nside, cat[self.ra_name],
-                          cat[self.dec_name], lonlat=True)
-        # Mask is binary, so 0.1 or 0.00001 doesn't really matter.
-        return cat[self.mask[ipix] > 0.1]
-
-    def _bin_z(self, cat):
-        return cat[(cat['ZPHOTO'] > self.z_edges[0]) &
-                   (cat['ZPHOTO'] <= self.z_edges[1])]
-
-    def _get_specsample(self, cat):
-        ids = cat['ZSPEC'] > -1
-        return cat[ids]
-
+    
+    
+    # ill need this in the future
     def get_nz(self, dz=0, return_jk_error=False):
-        if self.dndz is None:
-            f_exists, f_name = self._check_rerun_file_exists('nz_2MPZ.npz')
-            # Read from file if it exists
-            if f_exists:
-                d = np.load(f_name)
-                zm = d['z_mid']
-                nz = d['nz']
-                nz_jk = d['nz_jk']
-            else:  # Else compute DIR N(z) and jackknife resamples
-                c_p = self.get_catalog()
-                c_s = self._get_specsample(c_p)
-                # Sort spec sample by nested pixel index so jackknife
-                # samples are spatially correlated.
-                ip_s = hp.ring2nest(self.nside,
-                                    hp.ang2pix(self.nside,
-                                               c_s[self.ra_name],
-                                               c_s[self.dec_name],
-                                               lonlat=True))
-                idsort = np.argsort(ip_s)
-                c_s = c_s[idsort]
-                # Compute DIR N(z)
-                z, nz, nz_jk = get_DIR_Nz(c_s, c_p,
-                                          ['JCORR', 'KCORR', 'HCORR',
-                                           'W1MCORR', 'W2MCORR',
-                                           'BCALCORR', 'RCALCORR', 'ICALCORR'],
-                                          zflag='ZSPEC',
-                                          zrange=[0, 0.4],
-                                          nz=100,
-                                          njk=self.config.get('n_jk_dir', 100))
-                zm = 0.5*(z[1:] + z[:-1])
-                if f_name is not None:
-                    np.savez(f_name, z_mid=zm, nz=nz, nz_jk=nz_jk)
-            self.dndz = (zm, nz, nz_jk)
-
-        z, nz, nz_jk = self.dndz
-        z_dz = z + dz
-        sel = z_dz >= 0
-        if return_jk_error:
-            njk = len(nz_jk)
-            enz = np.std(nz_jk, axis=0)*np.sqrt((njk-1)**2/njk)
-            return np.array([z_dz[sel], nz[sel], enz[sel]])
-        else:
-            return np.array([z_dz[sel], nz[sel]])
+        pass
+       
 
     def get_signal_map(self, apply_galactic_correction=True):
         if self.delta_map is None:
@@ -132,16 +74,38 @@ class MapperNVSS(MapperBase):
             goodpix = self.mask > 0
             # Division by mask not really necessary, since it's binary.
             d[goodpix] = nmap_data[goodpix]/(mean_n*self.mask[goodpix])-1
-            self.delta_map = d
+            self.delta_map = d   
         return [self.delta_map]
 
     def get_mask(self):
         if self.mask is None:
-            self.mask = hp.ud_grade(hp.read_map(self.config['mask'],
-                                                verbose=False),
-                                    nside_out=self.nside)
+            
+            self.mask = np.ones(self.npix)
+            
+            r = hp.Rotator(coord=['C','G']) 
+            
+            RApix, DEpix = hp.pix2ang(self.nside, np.arange(self.npix), lonlat=True)
+            lpix, bpix = r(RApix, DEpix, lonlat=True)
+            
+            # angular conditions
+            self.mask[ (DEpix < -40) | (np.fabs(bpix) < 5)] = 0
+            
+            # holes catalog 
+            maskcat = np.loadtxt(self.file_sourcemask)
+            
+            RAmask = maskcat[:,0]
+            DEmask = maskcat[:,1]
+            radiusmask = maskcat[:,2]
+            vecmask = hp.ang2vec(RAmask,DEmask, lonlat=True)
+        
+            for vec, radius in zip(vecmask, radiusmask):
+                ipix_hole = hp.query_disc(nside, vec, np.radians(radius), inclusive=True)
+                self.mask[ipix_hole] = 0
+            
         return self.mask
-
+    
+    
+    # look at this function later
     def get_nl_coupled(self):
         if self.nl_coupled is None:
             self.cat_data = self.get_catalog()
@@ -155,6 +119,7 @@ class MapperNVSS(MapperBase):
             self.nl_coupled = N_ell * np.ones((1, 3*self.nside))
         return self.nl_coupled
 
+    # for all mappers
     def get_dtype(self):
         return 'galaxy_density'
 
