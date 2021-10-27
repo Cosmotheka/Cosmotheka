5d3ff8a3b74e80b7d764cf5fd1ff6180631523fa
Maquiner <jaimerz011235813@gmail.com>
2021-10-27 14:57:49 +0100

    get_coupled_nl support

--- xcell/mappers/mapper_ACTDR4_base.py
+++ xcell/mappers/mapper_ACTDR4_base.py
@@ -15,14 +15,18 @@ class MapperACTDR4Base(MapperBase):
         self._get_defaults(config)
         self.file_map = config['file_map']
         self.file_mask = config['file_mask']
+        self.file_noise = config.get('file_noise', None)
+        self.file_cross_noise = config.get('file_cross_noise', None)
         self.lmax = config.get('lmax', 6000)
         self.signal_map = None
         self.mask = None
         self.pixell_mask = None
         self.noise = None
         self.cross_noise = None
+        self.nl_coupled = None
         self.weights = None
         self.beam_info = None
+        self.beam = None
 
     def get_signal_map(self):
         return NotImplementedError("Do not use base class")
@@ -31,20 +35,20 @@ class MapperACTDR4Base(MapperBase):
         return NotImplementedError("Do not use base class")
 
     def get_noise(self):
-        if self.pixell_mask is None:
-            self.mask = enmap.read_map(self.file_mask)
-            self.mask = reproject.healpix_from_enmap(self.mask,
+        if self.noise is None:
+            self.noise = enmap.read_map(self.file_noise)
+            self.noise = reproject.healpix_from_enmap(self.noise,
                                                      lmax = self.lmax,
                                                      nside = self.nside)
-        return self.mask
+        return self.noise
     
     def get_cross_noise(self):
-        if self.mask is None:
-            self.mask = enmap.read_map(self.file_mask)
-            self.mask = reproject.healpix_from_enmap(self.mask,
+        if self.cross_noise is None:
+            self.cross_noise = enmap.read_map(self.file_cross_noise)
+            self.cross_noise = reproject.healpix_from_enmap(self.cross_noise,
                                                      lmax = self.lmax,
                                                      nside = self.nside)
-        return self.mask
+        return self.cross_noise
     
     def get_weights(self):
         if self.mask is None:
@@ -53,6 +57,14 @@ class MapperACTDR4Base(MapperBase):
                                                      lmax = self.lmax,
                                                      nside = self.nside)
         return self.mask
+    
+    def get_nl_coupled(self):
+        if self.nl_coupled is None:
+            noise = self.get_noise()
+            noise_f = self._get_nmt_field(signal=noise)
+            self.nl_coupled = nmt.compute_coupled_cell(noise_f, noise_f)
+        return self.nl_coupled
+           
     
     def _beam_gaussian(self, ell, fwhm_amin):
         sigma_rad = np.radians(fwhm_amin / 2.355 / 60)
@@ -65,5 +77,6 @@ class MapperACTDR4Base(MapperBase):
             else:
                 ell = np.arange(3*self.nside)
                 self.beam = self._beam_gaussian(ell, self.beam_info)
+                self.beam /= self.beam[0]
         return self.beam
 
--- xcell/mappers/mapper_Planck_base.py
+++ xcell/mappers/mapper_Planck_base.py
@@ -99,7 +99,6 @@ class MapperPlanckBase(MapperBase):
             else:
                 ell = np.arange(3*self.nside)
                 self.beam = self._beam_gaussian(ell, self.beam_info)
-                self.beam /= self.beam[0] #Apparently you have to normalize it
         return self.beam
 
     def get_spin(self):
