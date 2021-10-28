b1597823390dbd526cac45fad72c39b2d8de36be
David Alonso <dam.phys@gmail.com>
2021-10-28 13:54:58 +0100

    nl_coupled test

--- xcell/tests/test_mapper_rosat.py
+++ xcell/tests/test_mapper_rosat.py
@@ -6,7 +6,7 @@ import os
 
 
 def get_config():
-    c = {'exposure_map': 'xcell/tests/data/msk_rosat.fits',
+    c = {'exposure_map': 'xcell/tests/data/exp_rosat.fits',
          'photon_list': 'xcell/tests/data/cat_rosat.fits',
          'external_mask': 'xcell/tests/data/msk_rosat.fits',
          'energy_range': [0.5, 3.0],
@@ -22,16 +22,18 @@ def make_rosat_data(e0=0., ef=4., expval=200.):
     ra, dec = hp.pix2ang(nside, np.arange(npix), lonlat=True)
     ener = e0 + (ef-e0)*np.random.rand(npix)
     exp = np.ones(npix)*expval
+    msk = np.ones(npix)
     cat = Table({'raj2000': ra,
                  'dej2000': dec,
                  'energy_cor': ener,
                  'exposure_time': exp})
     cat.write('xcell/tests/data/cat_rosat.fits', overwrite=True)
-    hp.write_map('xcell/tests/data/msk_rosat.fits', exp, overwrite=True)
+    hp.write_map('xcell/tests/data/exp_rosat.fits', exp, overwrite=True)
+    hp.write_map('xcell/tests/data/msk_rosat.fits', msk, overwrite=True)
 
 
 def clean_rosat_data():
-    for fn in ['cat_rosat.fits', 'msk_rosat.fits']:
+    for fn in ['cat_rosat.fits', 'msk_rosat.fits', 'exp_rosat.fits']:
         fname = f'xcell/tests/data/{fn}'
         if os.path.isfile(fname):
             os.remove(fname)
@@ -83,4 +85,15 @@ def test_ener_cut():
     m = xc.mappers.MapperROSATXray(c)
     cat = m.get_pholist()
     assert len(cat) == npix
+    clean_rosat_data()
+
+
+def test_nl_coupled():
+    c = get_config()
+    make_rosat_data(e0=1., ef=1.5)
+    m = xc.mappers.MapperROSATXray(c)
+    nl = m.get_nl_coupled()
+    apix = hp.nside2pixarea(c['nside'])
+    nl_guess = 1/(200.**2*apix)
+    assert np.all(np.fabs(nl/nl_guess-1) < 1E-10)
     clean_rosat_data()
