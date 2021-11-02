0bb3a8fddb51a15a4882fecdc9408b96071d37ab
Maquiner <jaimerz011235813@gmail.com>
2021-11-02 16:44:23 +0000

    CIB upd

--- xcell/mappers/mapper_P15CIB.py
+++ xcell/mappers/mapper_P15CIB.py
@@ -16,21 +16,29 @@ class MapperP15CIB(MapperPlanckBase):
 
     def _get_hm_maps(self):
         if self.hm1_map is None:
-            hm1_map = hp.read_map(self.file_map, 1)
+            hm1_map = hp.read_map(self.file_hm1)
             self.hm1_map = [hp.ud_grade(hm1_map,
                             nside_out=self.nside)]
         if self.hm2_map is None:
-            hm2_map = hp.read_map(self.file_map, 2)
+            hm2_map = hp.read_map(self.file_hm2)
             self.hm2_map = [hp.ud_grade(hm2_map,
                             nside_out=self.nside)]
         return self.hm1_map, self.hm2_map
 
     def get_mask(self):
         if self.mask is None:
-            mask = hp.read_map(self.file_mask)
-            mask = hp.ud_grade(mask,
-                               nside_out=self.nside)
-            self.mask = mask
+            self.mask = np.ones(12*self.nside**2)
+            if self.file_gp_mask is not None:
+                field = self.gal_mask_modes[self.gal_mask_mode]
+                mask = hp.read_map(self.file_gp_mask, field)
+                mask = hp.ud_grade(mask,
+                                   nside_out=self.nside)
+                self.mask *= mask
+            if self.file_sp_mask is not None:
+                mask = hp.read_map(self.file_sp_mask)
+                mask = hp.ud_grade(mask,
+                                   nside_out=self.nside)
+                self.mask *= mask
         return self.mask
 
     def get_dtype(self):
