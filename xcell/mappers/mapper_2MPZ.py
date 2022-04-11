fa5ae522820075b9551cf582f4c9f3d86a823c37
David Alonso <dam.phys@gmail.com>
2022-04-11 10:37:23 +0100

    leftover print

--- xcell/mappers/mapper_2MPZ.py
+++ xcell/mappers/mapper_2MPZ.py
@@ -31,7 +31,6 @@ class Mapper2MPZ(MapperBase):
 
     def _get_coords(self, config):
         if self.coords == 'G':  # Galactic
-            print("HIII")
             return 'L', 'B'
         elif self.coords == 'C':  # Celestial/Equatorial
             return 'SUPRA', 'SUPDEC'
