475dc54b99f3363348412610fc663dbfa3cb430d
David Alonso <dam.phys@gmail.com>
2022-08-02 11:36:43 +0100

    flake failed

--- xcell/mappers/mapper_IceCube.py
+++ xcell/mappers/mapper_IceCube.py
@@ -7,7 +7,10 @@ from scipy.interpolate import interp1d
 
 
 class MapperIceCube(MapperBase):
-    def __init__(self, config, logE_ranges=[np.log10(300), np.log10(300)+1, np.log10(300)+2, np.log10(300)+3]):
+    def __init__(self, config, logE_ranges=[np.log10(300),
+                                            np.log10(300)+1,
+                                            np.log10(300)+2,
+                                            np.log10(300)+3]):
 
         self._get_defaults(config)
         self.npix = hp.nside2npix(self.nside)
