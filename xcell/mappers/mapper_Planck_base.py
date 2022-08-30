PS masking fix

xcell/mappers/mapper_Planck_base.py
@@ -18,6 +18,8 @@ class MapperPlanckBase(MapperBase):
         self.file_mask = config.get('file_mask', None)
         self.file_gp_mask = config.get('file_gp_mask', None)
         self.file_ps_mask = config.get('file_ps_mask', None)
+        self.ps_mask_modes = []
+        self.ps_mask_mode = {}
         self.signal_map = None
         self.hm1_map = None
         self.hm2_map = None
@@ -26,7 +28,6 @@ class MapperPlanckBase(MapperBase):
         self.cl_coupled = None
         self.cls_cov = None
         self.custom_auto = True
-        self.pre_mask_ps = config.get("pre_mask_ps", False)
         self.ps_mask = None
 
     def get_signal_map(self):
@@ -34,14 +35,8 @@ class MapperPlanckBase(MapperBase):
             signal_map = hp.read_map(self.file_map)
             signal_map[signal_map == hp.UNSEEN] = 0.0
             signal_map[np.isnan(signal_map)] = 0.0
-            if self.pre_mask_ps:
-                if self.file_ps_mask is not None:
-                    ps_mask = self._get_ps_mask()
-                    signal_map *= ps_mask
-                else:
-                    NotImplementedError("""Tried to pre-mask point
-                                        sources but couldn't find
-                                        file_ps_mask""")
+            ps_mask = self._get_ps_mask()
+            signal_map *= ps_mask
             signal_map = rotate_map(signal_map, self.rot)
             self.signal_map = np.array([hp.ud_grade(signal_map,
                                         nside_out=self.nside)])
@@ -58,7 +53,7 @@ class MapperPlanckBase(MapperBase):
                 msk = gp_mask
             else:
                 msk *= gp_mask
-        if (self.file_ps_mask is not None) & (self.pre_mask_ps is False):
+        if self.file_ps_mask is not None:
             ps_mask = self._get_ps_mask()
             if msk is None:
                 msk = ps_mask
@@ -72,9 +67,10 @@ class MapperPlanckBase(MapperBase):
     def _get_ps_mask(self):
         if self.ps_mask is None:
             self.ps_mask = np.ones(self.npix)
-            for mode in self.ps_mask_mode:
-                field = self.ps_mask_modes[mode]
-                self.ps_mask *= hp.read_map(self.file_ps_mask, field)
+            if self.file_ps_mask is not None:
+                for mode in self.ps_mask_mode:
+                    field = self.ps_mask_modes[mode]
+                    self.ps_mask *= hp.read_map(self.file_ps_mask, field)
         return self.ps_mask
 
     def _get_hm_maps(self):

xcell/mappers/mapper_SPT.py
@@ -20,21 +20,14 @@ class MapperSPT(MapperPlanckBase):
         self.ps_mask_mode = config.get('ps_mask_mode', ['default'])
         # Fix rotation from Planck's default
         self.rot = self._get_rotator('C')
-        self.pre_mask_ps = config.get("pre_mask_ps", True)
 
     def _get_hm_maps(self):
         if self.hm1_map is None:
             hm1_map = hp.read_map(self.file_hm1)
             hm1_map[hm1_map == hp.UNSEEN] = 0.0
             hm1_map[np.isnan(hm1_map)] = 0.0
-            if self.pre_mask_ps is True:
-                if self.file_ps_mask is not None:
-                    ps_mask = self._get_ps_mask()
-                    hm1_map *= ps_mask
-                else:
-                    NotImplementedError("""Tried to pre-mask point
-                                        sources but couldn't find
-                                        file_ps_mask""")
+            ps_mask = self._get_ps_mask()
+            hm1_map *= ps_mask
             hm1_map = rotate_map(hm1_map, self.rot)
             self.hm1_map = [hp.ud_grade(hm1_map,
                             nside_out=self.nside)]
@@ -42,14 +35,8 @@ class MapperSPT(MapperPlanckBase):
             hm2_map = hp.read_map(self.file_hm2)
             hm2_map[hm2_map == hp.UNSEEN] = 0.0
             hm2_map[np.isnan(hm2_map)] = 0.0
-            if self.pre_mask_ps is True:
-                if self.file_ps_mask is not None:
-                    ps_mask = self._get_ps_mask()
-                    hm2_map *= ps_mask
-                else:
-                    NotImplementedError("""Tried to pre-mask point
-                                        sources but couldn't find
-                                        file_ps_mask""")
+            ps_mask = self._get_ps_mask()
+            hm2_map *= ps_mask
             hm2_map = rotate_map(hm2_map, self.rot)
             self.hm2_map = [hp.ud_grade(hm2_map,
                             nside_out=self.nside)]