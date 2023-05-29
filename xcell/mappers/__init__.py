05bc6f9b3c470e2d34ea21b4815a82cafbe0f3fb
David Alonso <dam.phys@gmail.com>
2023-05-29 18:18:10 +0100

    all added

--- xcell/mappers/__init__.py
+++ xcell/mappers/__init__.py
@@ -26,6 +26,7 @@ from .mapper_NVSS import MapperNVSS
 from .mapper_CatWISE import MapperCatWISE
 from .mapper_ROSAT import MapperROSATXray
 from .mapper_dummy import MapperDummy
+from .mapper_GAIAQSO import MapperGAIAQSO
 from .utils import (get_map_from_points, get_DIR_Nz,
                     get_rerun_data, save_rerun_data,
                     rotate_mask, rotate_map)
