9af8e745245345ee0a952c7c1284671e735ca08c
carlosggarcia <carlos.garcia-garcia@physics.ox.ac.uk>
2024-04-22 10:37:25 +0100

    mappers: fix hsc

--- cosmotheka/mappers/mapper_HSC_DR1wl.py
+++ cosmotheka/mappers/mapper_HSC_DR1wl.py
@@ -100,7 +100,7 @@ class MapperHSCDR1wl(MapperBase):
                   cat[f'{isn}_derived_shear_bias_c1']) / (1 + mhat)
             e2 = (cat[f'{isn}_e2']/(2.*resp) -
                   cat[f'{isn}_derived_shear_bias_c2']) / (1 + mhat)
-            cat['e1'] = -e1
+            cat['e1'] = e1
             cat['e2'] = e2
 
             # Remove unnecessary columns
@@ -196,7 +196,7 @@ class MapperHSCDR1wl(MapperBase):
         cat = self.get_catalog()
         we1, we2 = get_map_from_points(cat, self.nside,
                                        w=cat[self.w_name],
-                                       qu=[cat['e1'], cat['e2']],
+                                       qu=[-cat['e1'], -cat['e2']],
                                        ra_name='ra',
                                        dec_name='dec',
                                        rot=self.rot)
