df9eb6db67312611e5daaed8a95c7ece2cef8a65
adrien-laposta <adrien.la.posta@gmail.com>
2025-06-24 11:37:13 +0100

    flake

--- cosmotheka/mappers/mapper_ACTDR6tSZ.py
+++ cosmotheka/mappers/mapper_ACTDR6tSZ.py
@@ -32,10 +32,14 @@ class MapperACTDR6tSZ(MapperACTCompSept):
         if self.inpaint_mask_file is not None:
             self.mask_name = f"{self.mask_name}_inpaint"
             ps_in = True
-        
-        self.mask_name = f"{self.mask_name}_apo{self.footprint_aposize:.1f}arcmin"
+
+        self.mask_name = (
+            f"{self.mask_name}_apo{self.footprint_aposize:.1f}arcmin"
+        )
         if ps_in:
-            self.mask_name = f"{self.mask_name}_psapod{self.ps_aposize:.1f}arcmin"
+            self.mask_name = (
+                f"{self.mask_name}_psapod{self.ps_aposize:.1f}arcmin"
+            )
 
     def _get_src_binary_mask(self, file):
         msk = enmap.read_map(file)
@@ -72,23 +76,23 @@ class MapperACTDR6tSZ(MapperACTCompSept):
         if self.inpaint_mask_file is not None:
             inpaint_mask = self._get_src_binary_mask(self.inpaint_mask_file)
             src_mask *= inpaint_mask
-        
+
         if np.any(src_mask < 1):
             src_mask = nmt.mask_apodization(
                 src_mask,
-                aposize=self.ps_aposize/60, # Convert to deg
+                aposize=self.ps_aposize/60,  # Convert to deg
                 apotype="C1"
             )
         msk = nmt.mask_apodization(
             msk,
-            aposize=self.footprint_aposize/60, # Convert to deg
+            aposize=self.footprint_aposize/60,  # Convert to deg
             apotype="C1"
         )
         msk *= src_mask
-        return msk      
+        return msk
 
     def get_dtype(self):
         return 'cmb_tSZ'
 
-    def get_spin(self): 
+    def get_spin(self):
         return 0
