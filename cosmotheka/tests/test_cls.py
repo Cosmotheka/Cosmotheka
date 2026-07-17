7ed89453bb1109b057758bee9e3daa11d074520c
carlosggarcia <carlos.garcia-garcia@physics.ox.ac.uk>
2026-07-17 08:58:51 +0100

    added comment for missing tests

--- cosmotheka/tests/test_cls.py
+++ cosmotheka/tests/test_cls.py
@@ -250,6 +250,11 @@ def test_cov_nlmarg():
     _clean_tmpdir(tmpdir2)
 
 
+# TODO: Missing test for:
+#       a) 1h+2h combinations
+#       b) computation of missing terms. E.g. 1h is already in but the user
+#          requests 1h+2h later on. It only computes 2h and add it to the 1h
+#          contribution already there.
 @pytest.mark.parametrize("kind", ["1h", "2h", "3h", "4h", None])
 def test_cov_ng(kind):
     # From CCL directly
