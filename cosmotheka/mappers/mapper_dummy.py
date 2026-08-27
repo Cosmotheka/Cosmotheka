ff4a62f9f36f0021890d8e886d124278d89cbb41
damonge <dam.phys@gmail.com>
2026-08-27 19:46:10 +0100

    testing

--- cosmotheka/mappers/mapper_dummy.py
+++ cosmotheka/mappers/mapper_dummy.py
@@ -180,11 +180,12 @@ class MapperDummy(MapperBase):
 
     def get_cls_covar_coupled(self):
         if self.cls_cov is None:
-            clc = self.get_cl_coupled()
-            self.cls_cov = {'cross': clc-self.custom_offset,
-                            'auto_11': clc,
-                            'auto_12': clc-self.custom_offset,
-                            'auto_22': clc}
+            fld = self.get_nmt_field()
+            clcov = nmt.get_iNKA_cell(fld, fld)
+            self.cls_cov = {'cross': clcov-self.custom_offset,
+                            'auto_11': clcov,
+                            'auto_12': clcov-self.custom_offset,
+                            'auto_22': clcov}
         return self.cls_cov
 
     def get_dtype(self):
